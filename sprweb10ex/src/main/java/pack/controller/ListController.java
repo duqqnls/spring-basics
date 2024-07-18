139dcded060e3a8210619a9224d1aca08d10ed70
yeobin <duqqnls123@naver.com>
2024-07-18 09:34:02 +0900

    [MY BATIS]

---
+++ sprweb10ex/src/main/java/pack/controller/ListController.java
@@ -0,0 +1,35 @@
+package pack.controller;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+
+import pack.model.DataDao;
+import pack.model.JikwonDto;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class ListController {
+	
+	@Autowired
+	private DataDao dataDao;
+	
+	@GetMapping("/")
+	public String start() {
+		return "index";
+	}
+	
+	@GetMapping("jikwondb")
+	public String listProcess(@RequestParam(value="jik")String jik , Model model) {
+		ArrayList<JikwonDto> list = (ArrayList<JikwonDto>)dataDao.selectJikList(jik);
+		model.addAttribute("datas", list);
+	
+		return "show";
+	}
+	
+}
