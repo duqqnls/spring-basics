10bf4006039bf0edaad239c4ce12eae255f45923
yeobin <duqqnls123@naver.com>
2024-07-19 10:03:02 +0900

    [JPA] Repository

--- sprweb14jpa/src/main/java/pack/model/ProductCrudRepository.java
+++ sprweb14jpa/src/main/java/pack/model/ProductCrudRepository.java
@@ -2,7 +2,6 @@ package pack.model;
 
 import java.util.List;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,14 +9,14 @@ import org.springframework.data.repository.query.Param;
 public interface ProductCrudRepository extends JpaRepository<ProductVo, Integer>{
 	// CrudRepository > PagingAndSortingRepository > JpaRepository		
 	
-	// JPQL
-	@Query(value = "select p from ProductVo as p")
-	List<ProductVo> findAllData();
-	
 	// 메소드 이름으로 쿼리 생성 방법 find + (엔티티 이름) + By + 변수 이름 
 	// 엔티티의 이름은 생략이 가능
 	ProductVo findByCode(Integer code);
 	
+	// JPQL
+	@Query(value = "select p from ProductVo as p")
+	List<ProductVo> findAllData();
+
 	// where 조건
 	@Query(value = "select p from ProductVo as p where p.code=:code")
 	ProductVo findByCodeMy(@Param("code") int code);
@@ -28,4 +27,7 @@ public interface ProductCrudRepository extends JpaRepository<ProductVo, Integer>
 	@Query(value = "select p from ProductVo as p where p.code =?1 or p.sang=?2")
 	List<ProductVo> findByData(int code, String sang);	
 
+	// native Query문 사용
+	@Query(value= "select code,sang,su,dan from product where code <= 5", nativeQuery = true) // 기본은 false ~
+	List<ProductVo> findAllData2();
 }
