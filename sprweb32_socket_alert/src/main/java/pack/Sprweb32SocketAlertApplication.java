018c2ba4e958287b45363b7a4c12b2798544d8da
duqqnls <duqqnls123@naver.com>
2024-09-05 18:28:02 +0900

    [SOCKET_ALERT]

---
+++ sprweb32_socket_alert/src/main/java/pack/Sprweb32SocketAlertApplication.java
@@ -0,0 +1,13 @@
+package pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sprweb32SocketAlertApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sprweb32SocketAlertApplication.class, args);
+	}
+
+}
---
+++ sprweb32_socket_alert/src/main/java/pack/model/MyNotification.java
@@ -0,0 +1,28 @@
+package pack.model;
+
+public class MyNotification {
+
+	private String type; 		// 알림 유형 : 친구 요청, 댓글, 좋아요
+	private String message;		// 알림 내용
+	
+	public MyNotification() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
