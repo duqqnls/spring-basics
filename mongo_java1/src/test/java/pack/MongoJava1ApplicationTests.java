58abdeed2a907386aeb349433d0e52fc26956efb
duqqnls <duqqnls123@naver.com>
2024-09-19 13:05:02 +0900

    MongoDB 연동

---
+++ mongo_java1/src/test/java/pack/MongoJava1ApplicationTests.java
@@ -0,0 +1,13 @@
+package pack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoJava1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
